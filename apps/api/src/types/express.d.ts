2a704b1b47dde48d0b8e6f382743302223a48089
NyverZ <nyverzthurfah@gmaail.com>
2026-04-01 23:10:37 +0800

    ex

--- apps/api/src/types/express.d.ts
+++ apps/api/src/types/express.d.ts
@@ -2,11 +2,22 @@ import "express";
 
 declare global {
   namespace Express {
+    interface MulterFile {
+      fieldname: string;
+      originalname: string;
+      encoding: string;
+      mimetype: string;
+      size: number;
+      buffer: Buffer;
+    }
+
     interface Request {
       user?: {
         id: string;
         username: string;
       };
+      file?: MulterFile;
+      files?: MulterFile[] | { [fieldname: string]: MulterFile[] };
     }
   }
 }
